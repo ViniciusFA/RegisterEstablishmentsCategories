bbf1aa6bc758cb31f7838016575d362a1c0750a9
Vinicius Andrade <vinny_fa2000@hotmail.com>
2021-01-18 00:58:42 -0300

    Creating the Layer Business that will be responsible to do the business rule  and take out  the responsability of the controller layer.

---
+++ Code/RegisterEstablishmentsCategories/Business/Base/DefaultBusiness.cs
@@ -0,0 +1,60 @@
+﻿using DataAccess;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Business.Base
+{
+    public class DefaultBusiness 
+    {
+        public T GetSingle<T>(Context dbContext, Expression<Func<T, bool>> predicate) where T : class
+        {
+            return new DefaultDataAccess().GetSingle<T>(dbContext, predicate);
+        }
+
+        public T GetSingleOrDefault<T>(Context dbContext, Expression<Func<T, bool>> predicate) where T : class
+        {
+            return new DefaultDataAccess().GetSingleOrDefault<T>(dbContext, predicate);
+        }
+
+        public T GetFirst<T>(Context dbContext, Expression<Func<T, bool>> predicate) where T : class
+        {
+            return new DefaultDataAccess().GetFirst<T>(dbContext, predicate);
+        }
+
+        public T GetFirstOrDefault<T>(Context dbContext, Expression<Func<T, bool>> predicate) where T : class
+        {
+            return new DefaultDataAccess().GetFirstOrDefault<T>(dbContext, predicate);
+        }
+
+        public IQueryable<T> GetList<T>(Context dbContext, Expression<Func<T, bool>> predicate) where T : class
+        {
+            return new DefaultDataAccess().GetList<T>(dbContext, predicate);
+        }
+
+        public IQueryable<T> GetList<T>(Context dbContext) where T : class
+        {
+            return new DefaultDataAccess().GetList<T>(dbContext);
+        }
+
+        public void Insert<T>(Context dbContext, T model, bool saveChanges) where T : class
+        {
+            new DefaultDataAccess().Insert<T>(dbContext, model, saveChanges);
+        }
+
+        public void Update<T>(Context dbContext, T model, bool saveChanges = true) where T : class
+        {
+            new DefaultDataAccess().Update<T>(dbContext, model, saveChanges);
+        }
+
+        public void Exclude<T>(Context dbContext, T model, bool saveChanges = true) where T : class
+        {
+            new DefaultDataAccess().Exclude<T>(dbContext, model, saveChanges);
+        }
+        
+        public bool SaveChanges(Context dbContext)
+        {
+            return new DefaultDataAccess().SaveChanges(dbContext);
+        }
+    }
+}
---
+++ Code/RegisterEstablishmentsCategories/Business/EstablishmentsBusiness.cs
@@ -0,0 +1,42 @@
+﻿using Business.Base;
+using DataAccess;
+using Models;
+using Models.ViewModel;
+using PagedList;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Business
+{
+    public class EstablishmentsBusiness : DefaultBusiness
+    {
+
+        public IPagedList<EstablishmentViewModel> GetAll(Context context, EstablishmentViewModel viewModel, int pageNumber, int pageSize)
+        {
+            var result = new List<EstablishmentViewModel>();
+            var listData = this.GetList<Establishment>(context);
+
+            listData.OrderBy(o => o.CompanyName)
+                    .ToList().ForEach(e =>
+                    {
+                        result.Add(new EstablishmentViewModel()
+                        {
+                            Address = e.Address,
+                            AgencyAccount = e.AgencyAccount,
+                            Category = e.Category,
+                            City = e.City,
+                            CNPJ = e.CNPJ,
+                            CompanyName = e.CompanyName,
+                            Email = e.Email,
+                            FantasyName = e.FantasyName,
+                            PhoneNumber = e.PhoneNumber.ToString(),
+                            RegisterDate = e.RegisterDate.ToString("dd/MM/yyyy"),
+                            State = e.State,
+                            Status = e.Status
+                        });
+                    });
+
+            return result.ToPagedList(pageNumber, pageSize);
+        }
+    }
+}

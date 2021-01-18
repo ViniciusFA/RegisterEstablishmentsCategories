4d9f9174b08ae27fd0197e452be1ba283679477f
Vinicius Andrade <vinny_fa2000@hotmail.com>
2021-01-18 00:52:09 -0300

    Creating the layer Utilities that's layer responsible to provider the resources sued in all application like AplicationConstants.

---
+++ Code/RegisterEstablishmentsCategories/Utilities/ApplicationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class ApplicationConstants
+    {
+        public const string HC_CBLEC_CONTEXT_INSTANCE = "HC_CBLEC_CONTEXT_INSTANCE";
+    }
+}
